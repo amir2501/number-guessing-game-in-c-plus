6001b9979cc7cdf5635738810f12667254bb7b97
Erkinov Amirbek <erkinovamir100@gmail.com>
2024-09-22 21:17:48 +0500

    Initial Commit

---
+++ Number guessing game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Number guessing game
+//
+//  Created by MacBook Pro M3 on 22/09/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
